ecb59ea9c1116226ac4b7de5f576862edf908f87
kavi <sumathi291098@gmail.com>
2023-04-19 20:26:20 +0530

    Adding a form

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from '../MealItem/MealItemForm.module.css';
+export default function MealItemForm() {
+  return (
+    <div>
+        <form className={classes.form}>
+        <input />
+        <button>+ Add</button>
+        </form>
+    </div>
+  )
+}
